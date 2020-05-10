6d8e63da8e708035b4d9c55ad3a5ea0b24787cdc
Omar ABDELKEFI <omar.abdelkafi.isims@gmail.com>
2020-05-10 21:04:38 +0100

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Main from "./components/MainComponent";
-
+import "./App.css"
 class App extends React.Component {
 
   render() {
 
     return (
-        <div >
+        <>
         <Main/>
-        </div>
+        </>
     );
   }
 }
